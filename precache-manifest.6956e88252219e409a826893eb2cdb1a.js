75dc01f36f3bb218956f44b2208945a699483e72
Anarodme <109930993+Anarodme@users.noreply.github.com>
2024-01-23 15:02:11 -0600

    Update precache-manifest.6956e88252219e409a826893eb2cdb1a.js

--- precache-manifest.6956e88252219e409a826893eb2cdb1a.js
+++ precache-manifest.6956e88252219e409a826893eb2cdb1a.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
     "revision": "d779204d6197eaba59b9",
-    "url": "/PortfolioWebsite-2023/static/css/main.eb29acd6.chunk.css"
+    "url": "static/css/main.eb29acd6.chunk.css"
   },
   {
     "revision": "d779204d6197eaba59b9",
-    "url": "/PortfolioWebsite-2023/static/js/main.45155b42.chunk.js"
+    "url": "static/js/main.45155b42.chunk.js"
   },
   {
     "revision": "c8e795fdd43870f774f1",
-    "url": "/PortfolioWebsite-2023/static/js/runtime~main.fe6b76da.js"
+    "url": "static/js/runtime~main.fe6b76da.js"
   },
   {
     "revision": "f8a5cd99cc4facb58d3f",
@@ -17,38 +17,38 @@ self.__precacheManifest = [
   },
   {
     "revision": "09929ddac84c0cbc6fe2269f626689ed",
-    "url": "/PortfolioWebsite-2023/static/media/about.09929dda.jpg"
+    "url": "/Portfolio-2024/static/media/about.09929dda.jpg"
   },
   {
     "revision": "bbf53d4e4065b4d4fd8cfe2991220d58",
-    "url": "/PortfolioWebsite-2023/static/media/resume2023.bbf53d4e.pdf"
+    "url": "/Portfolio-2024/static/media/resume2023.bbf53d4e.pdf"
   },
   {
     "revision": "7f91416f1836c747f49e6b39713ce261",
-    "url": "/PortfolioWebsite-2023/static/media/work1.7f91416f.jpg"
+    "url": "/Portfolio-2024/static/media/work1.7f91416f.jpg"
   },
   {
     "revision": "b855399263e7984f81521116704125b1",
-    "url": "/PortfolioWebsite-2023/static/media/work2.b8553992.jpg"
+    "url": "/Portfolio-2024/static/media/work2.b8553992.jpg"
   },
   {
     "revision": "a139c43dc16adc67035a48fcb1955bdc",
-    "url": "/PortfolioWebsite-2023/static/media/work3.a139c43d.jpg"
+    "url": "/Portfolio-2024/static/media/work3.a139c43d.jpg"
   },
   {
     "revision": "09b0176f1df222939a6e3a48454a43db",
-    "url": "/PortfolioWebsite-2023/static/media/work4.09b0176f.jpg"
+    "url": "/Portfolio-2024/static/media/work4.09b0176f.jpg"
   },
   {
     "revision": "cc24c06693105e4c32e6d07bb376c8ae",
-    "url": "/PortfolioWebsite-2023/static/media/work5.cc24c066.jpg"
+    "url": "/Portfolio-2024/static/media/work5.cc24c066.jpg"
   },
   {
     "revision": "09929ddac84c0cbc6fe2269f626689ed",
-    "url": "/PortfolioWebsite-2023/static/media/profile.09929dda.jpg"
+    "url": "/Portfolio-2024/static/media/profile.09929dda.jpg"
   },
   {
     "revision": "5ddfaeb8f4696d390b7a051605d1a3cd",
-    "url": "/PortfolioWebsite-2023/index.html"
+    "url": "/Portfolio-2024/index.html"
   }
 ];
