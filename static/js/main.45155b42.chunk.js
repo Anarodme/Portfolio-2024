ff602e9d3c43a7955b080018e847e7c6ce134832
Anarodme <109930993+Anarodme@users.noreply.github.com>
2024-01-23 18:11:41 -0600

    Update main.45155b42.chunk.js

--- static/js/main.45155b42.chunk.js
+++ static/js/main.45155b42.chunk.js
@@ -6,7 +6,7 @@
     ,
     ,
     function (e, a, t) {
-        e.exports = t.p + "static/media/me-icon-smiling.5abe33fbd03d140d5821.png";
+        e.exports = t.p + "static/media/me-icon-smiling.png";
     },
     function (e, a, t) {
       e.exports = t.p + "static/media/profile.jpg";
